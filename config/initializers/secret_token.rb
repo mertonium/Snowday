9dd5cbc3b7692d2fe1a3c85b4eb944c48783bafb
John Mertens <john.mertens@gmail.com>
2011-08-15 19:15:13 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snowday::Application.config.secret_token = '49f9b4a8ebc50b6deefcd6ce3d295fd1a23077717fa2036f0d978f266c56bfdcf27259f91953616a4aefd10009b5c9e628af1baa27b50274dda430ac9fc0d2d9'
