ff47c592d4f18faf8ff11e6c18e1adad856ebe09
John Mertens <john.mertens@gmail.com>
2011-08-22 16:55:42 -0700

    Added simplecov gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -26,5 +28,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   
   # For devise testing
+  config.include Devise::TestHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
 end
